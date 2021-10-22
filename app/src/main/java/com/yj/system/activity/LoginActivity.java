f931c2616e34ef5e0f63a93b47488947343b5fba
ZSAndroid <1210762604@qq.com>
2021-10-22 19:50:13 +0800

    校园网实现秒登录

--- app/src/main/java/com/yj/system/activity/LoginActivity.java
+++ app/src/main/java/com/yj/system/activity/LoginActivity.java
@@ -50,6 +50,8 @@ public class LoginActivity extends AppCompatActivity {
     private Button btGo;
 
     private String username,password;
+    private Call call;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,7 +78,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void Login() {
-        LoadingDialog.showSimpleLD(LoginActivity.this,getString(R.string.loading));
+
         username = etUsername.getText().toString().trim();
         password = etPassword.getText().toString().trim();
         if (username.isEmpty()) {
@@ -84,6 +86,7 @@ public class LoginActivity extends AppCompatActivity {
             //设置Snackbar上提示的字体颜色
             setSnackBarMessageTextColor(snackbar, Color.parseColor("#FFFFFF"));
             snackbar.show();
+
             return;
         }
         if (password.isEmpty()) {
@@ -98,6 +101,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void doLoginSystem() {
+        LoadingDialog.showSimpleLD(LoginActivity.this,getString(R.string.loading));
         ClearableCookieJar cookie = new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(LoginActivity.this));
         Log.i(TAG, "cookie============: " + cookie);
         //1.创建OkHttpClient对象
@@ -113,12 +117,11 @@ public class LoginActivity extends AppCompatActivity {
                 .build();
 
         //new call
-        Call call = mOkHttpClient.newCall(request);
+        call = mOkHttpClient.newCall(request);
 
         JSONObject jsonObject = null;
         //5.请求加入调度,重写回调方法，异步方式请求加入调度
         call.enqueue(new Callback() {
-
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                 String res = response.body().string();
@@ -130,6 +133,7 @@ public class LoginActivity extends AppCompatActivity {
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
+                                    LoadingDialog.closeSimpleLD();
                                     Explode explode = new Explode();
                                     explode.setDuration(500);
                                     getWindow().setExitTransition(explode);
@@ -142,11 +146,13 @@ public class LoginActivity extends AppCompatActivity {
                                 }
                             });
 
-                        } else if (jsonObject.optString("state").equals("0") && jsonObject.optString("msg").equals("用户不存在")) {
+                        } else if (jsonObject.optString("state").equals("0") && jsonObject.optString("msg").equals("密码错误")) {
                             Snackbar snackbar = Snackbar.make(btGo, "用户名和密码不匹配！", Snackbar.LENGTH_LONG);
                             //设置Snackbar上提示的字体颜色
                             setSnackBarMessageTextColor(snackbar, Color.parseColor("#FFFFFF"));
                             snackbar.show();
+                            LoadingDialog.closeSimpleLD();
+
                         }
                     } catch (JSONException e) {
                         e.printStackTrace();
@@ -154,18 +160,33 @@ public class LoginActivity extends AppCompatActivity {
                 } else {
                     throw new IOException("发生错误:" + response.body().string());
                 }
-
             }
 
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                Log.d(TAG, "onFailure: " + e.getMessage());
+                Snackbar snackbar = Snackbar.make(btGo, "登录失败，服务器连接超时！", Snackbar.LENGTH_LONG);
+                //设置Snackbar上提示的字体颜色
+                setSnackBarMessageTextColor(snackbar, Color.parseColor("#FFFFFF"));
+                snackbar.show();
+                LoadingDialog.closeSimpleLD();
             }
         });
+
     }
 
     public void setSnackBarMessageTextColor(Snackbar snackbar, int color) {
         View view = snackbar.getView();
         ((TextView) view.findViewById(R.id.snackbar_text)).setTextColor(color);
     }
+
+    /**
+     * 按下返回键
+     */
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        call.cancel();
+        LoadingDialog.disDialog();
+    }
+
 }
--- app/src/main/java/com/yj/system/activity/LoginSuccessActivity.java
+++ app/src/main/java/com/yj/system/activity/LoginSuccessActivity.java
@@ -5,63 +5,82 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Explode;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.yj.system.ui.Wave.Wave;
 import com.yj.system.ui.Wave.WaveView;
 import com.yj.system.utils.StatusBarUtils;
+import com.yj.system.utils.dialog.LoadingDialog;
 import com.yj.systemc.R;
+
+import java.util.Date;
 
 
 /**
  * created by on 2021/10/21
- * 描述：
+ * 描述：登录成功界面
  *
  * @author ZSAndroid
  * @create 2021-10-21-21:56
  */
-public class LoginSuccessActivity extends AppCompatActivity {
+public class LoginSuccessActivity extends AppCompatActivity implements View.OnClickListener {
     private WaveView wave_view;//背景动画
-    private TextView tv_username;
-    private FloatingActionButton fab;
+    private TextView tv_username, tv_yj; //叶静毕设Lable标签+跑马灯登录用户名
+    private FloatingActionButton fab;//打卡动画按钮
+    private long firstTime;//3s再按一次返回
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_success);
+        initData();//准备数据
+        initView();//初始化View
+        setListener();//设置监听事件
+        initWave();
+
+    }
+
+    /**
+     * 页面加载+准备数据
+     */
+    private void initData() {
+        //设置沉浸式状态栏
+        StatusBarUtils.fullScreen(this);
         Explode explode = new Explode();
         explode.setDuration(500);
         getWindow().setExitTransition(explode);
         getWindow().setEnterTransition(explode);
+    }
 
-        StatusBarUtils.fullScreen(this);
 
-
-        tv_username = findViewById(R.id.tv_username);
-        tv_username.setText(getIntent().getStringExtra("username"));
+    private void initView() {
+        tv_username = findViewById(R.id.tv_username);//跑马灯登录用户名
         wave_view = (WaveView) findViewById(R.id.wave_view);//背景动画
         fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getWindow().setExitTransition(null);
-                getWindow().setEnterTransition(null);
-                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginSuccessActivity.this, fab, fab.getTransitionName());
-                startActivity(new Intent(LoginSuccessActivity.this, PunchActivity.class), options.toBundle());
-            }
-        });
-        initWave();
+        tv_yj = findViewById(R.id.tv_yj);
+        //优先级显示，作用--->不被背景动画遮挡，显示最顶层
+        tv_yj.bringToFront();
+        tv_username.bringToFront();
+        //设置友好时间提示
+        getDate(tv_username);
     }
-    
+
+    /**
+     * 设置监听事件
+     */
+    private void setListener() {
+        fab.setOnClickListener(this);//给浮动按钮(打卡)，设置监听
+    }
+
     /**
      * 初始化背景动画
      */
     private void initWave() {
-
         wave_view.setOrientation(WaveView.Orientation.DOWN);
         int color = Color.parseColor("#55303F9F");
         Wave wave1 = new Wave(1080, 90, 10, 130, color);
@@ -70,5 +89,76 @@ public class LoginSuccessActivity extends AppCompatActivity {
         wave_view.addWave(wave1);
         wave_view.addWave(wave2);
         wave_view.addWave(wave3);
+    }
+
+    /**
+     * 设置控件点击事件的执行逻辑
+     *
+     * @param view
+     */
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fab: //点击打卡按钮动画过度打卡界面
+                getWindow().setExitTransition(null);
+                getWindow().setEnterTransition(null);
+                //动画过渡开始
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginSuccessActivity.this, fab, fab.getTransitionName());
+                //同时执行跳转
+                startActivity(new Intent(LoginSuccessActivity.this, PunchActivity.class), options.toBundle());
+                break;
+        }
+    }
+
+    /**
+     * 判断当前时间处于那个时间
+     *
+     * @param tv
+     */
+    private void getDate(TextView tv) {
+        String loginUserName = getIntent().getStringExtra("username");
+        Date d = new Date();
+        if (d.getHours() < 4) {
+            tv.setText("                 早点休息哟~，" + loginUserName + "                 ");
+        } else if (d.getHours() < 11) {
+            tv.setText("                 早上好，" + loginUserName + "                 ");
+        } else if (d.getHours() < 14) {
+            tv.setText("                 中午好，" + loginUserName + "                 ");
+        } else if (d.getHours() < 18) {
+            tv.setText("                 下午好，" + loginUserName + "                 ");
+        } else if (d.getHours() < 24) {
+            tv.setText("                 晚上好，" + loginUserName + "                 ");
+        }
+    }
+
+
+
+    /**
+     * 防触碰处理
+     * 再按一次退出程序
+     */
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_BACK:
+                long secondTime = System.currentTimeMillis();
+                if (secondTime - firstTime > 3000) {
+                    Snackbar snackbar = Snackbar.make(wave_view, "再按一次退出程序！", Snackbar.LENGTH_LONG);
+                    //设置Snackbar上提示的字体颜色
+                    setSnackBarMessageTextColor(snackbar, Color.parseColor("#FFFFFF"));
+                    snackbar.show();
+                    firstTime = secondTime;
+                    return true;
+                } else {
+                    System.exit(0);
+                }
+                break;
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    public void setSnackBarMessageTextColor(Snackbar snackbar, int color) {
+        View view = snackbar.getView();
+        ((TextView) view.findViewById(R.id.snackbar_text)).setTextColor(color);
     }
 }
